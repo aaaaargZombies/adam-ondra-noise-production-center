5a3da7d9ba89d0d9bb4467bc2cfa04ca28c28213
aaaaargZombies <you@example.com>
2021-03-15 15:21:14 +0000

    Revert "Revert "add bar graph""
    This reverts commit 339c2c43918c6d4281b722e4e2cd90940bc56982.

---
+++ freqGraph.js
@@ -0,0 +1,74 @@
+let paths = [
+	"./audio/moan_01.mp3",
+	"./audio/moan_02.mp3",
+	"./audio/shreak_03.mp3",
+	"./audio/shreak_01.mp3",
+	"./audio/weird_03.mp3",
+	"./audio/weird_01.mp3",
+	"./audio/weird_04.mp3",
+	"./audio/weird_02.mp3",
+	"./audio/growl_01.mp3",
+	"./audio/growl_02.mp3",
+	"./audio/shortOoooh_01.mp3",
+	"./audio/shortOoooh_03.mp3",
+	"./audio/breath_08.mp3",
+	"./audio/percusive_02.mp3",
+	"./audio/percusive_03.mp3",
+	"./audio/percusive_01.mp3"
+];
+
+var barHeight;
+
+const $ = q => document.querySelector(q);
+const $$ = q => document.querySelectorAll(q);
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+let canvas = $("canvas");
+canvas.width = width;
+canvas.height = height;
+let ctx = canvas.getContext("2d");
+
+$("body").onkeydown = e => {
+	if (e.key === " ") {
+		e.preventDefault();
+		audio.play();
+	}
+};
+
+let audio = new Audio(paths[0]);
+
+let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+let analyser = audioCtx.createAnalyser();
+
+let source = audioCtx.createMediaElementSource(audio);
+source.connect(analyser);
+
+analyser.fftSize = 256;
+let bufferLength = analyser.frequencyBinCount;
+let dataArray = new Uint8Array(bufferLength);
+
+analyser.getByteTimeDomainData(dataArray);
+
+ctx.clearRect(0, 0, width, height);
+
+const draw = () => {
+	requestAnimationFrame(draw);
+
+	analyser.getByteFrequencyData(dataArray);
+
+	ctx.fillStyle = "rgb(0, 0, 0)";
+	ctx.fillRect(0, 0, width, height);
+
+	barWidth = (width / bufferLength) * 2.5;
+	var x = 0;
+
+	for (var i = 0; i < bufferLength; i++) {
+		barHeight = dataArray[i] / 2;
+
+		ctx.fillStyle = "rgb(" + (barHeight + 100) + ",50,50)";
+		ctx.fillRect(x, height - barHeight / 2, barWidth, barHeight);
+
+		x += barWidth + 1;
+	}
+};
---
+++ todo.md
@@ -0,0 +1,5 @@
+# TODO
+
+- make wave positive only
+- add multiple waves
+- is it possible to create fill and order the layers to obscure each other like the joy division cover
