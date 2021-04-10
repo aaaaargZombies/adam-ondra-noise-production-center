737813621a0cedd6dbddff887d81ee4880873b86
aaaaargZombies <you@example.com>
2021-04-10 12:02:10 +0100

    improve header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,10 @@ import styles from "./styles.module.css";
 class Header extends React.Component {
 	render() {
 		return (
-			<div className={styles.header}>
+			<header className={styles.header}>
 				<h1 className={styles.title}>A0NPC</h1>
 				{/* <h2 className={styles.info}>?</h2> */}
-			</div>
+			</header>
 		);
 	}
 }
