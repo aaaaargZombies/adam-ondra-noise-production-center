7071ea6228f1abbbb804396dbca7ba602ba8f739
aaaaargZombies <you@example.com>
2021-08-13 15:56:25 +0100

    change Header.js to a functional component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,13 @@
 import React from "react";
 import styles from "./styles.module.css";
-class Header extends React.Component {
-	render() {
-		return (
-			<header className={styles.header}>
-				<h1 className={styles.title}>A0NPC</h1>
-				<button className={styles.btn} onClick={() => this.props.showInfo()}>
-					{this.props.info ? "x" : "?"}
-				</button>
-			</header>
-		);
-	}
-}
+
+const Header = (props) => (
+	<header className={styles.header}>
+		<h1 className={styles.title}>A0NPC</h1>
+		<button className={styles.btn} onClick={() => props.showInfo()}>
+			{props.info ? "x" : "?"}
+		</button>
+	</header>
+);
 
 export default Header;
