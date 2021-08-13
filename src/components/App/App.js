4c1350bbf42b50a8ad388ec19834a85cc89dc11c
aaaaargZombies <you@example.com>
2021-08-13 15:52:01 +0100

    change App.js to use hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { info: false };
-		this.showInfo = this.showInfo.bind(this);
-	}
-
-	showInfo() {
-		this.setState({ info: !this.state.info });
-	}
-
-	render() {
-		return (
-			<div className={styles.App}>
-				<Header info={this.state.info} showInfo={this.showInfo} />
-				<Main info={this.state.info} />
-			</div>
-		);
-	}
-}
+const App = () => {
+	const [info, setInfo] = useState(false);
+	const showInfo = () => {
+		setInfo(!info);
+	};
+	return (
+		<div className={styles.App}>
+			<Header info={info} showInfo={showInfo} />
+			<Main info={info} />
+		</div>
+	);
+};
 
 export default App;
