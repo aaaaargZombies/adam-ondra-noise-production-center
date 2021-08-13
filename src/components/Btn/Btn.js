f1f977a64d12bf16215db8246ecad5d1ebcf6fc6
aaaaargZombies <you@example.com>
2021-08-13 17:38:12 +0100

    change Btn.js to a functional component

--- src/components/Btn/Btn.js
+++ src/components/Btn/Btn.js
@@ -1,11 +1,8 @@
 import React from "react";
 import styles from "./styles.module.css";
-class Btn extends React.Component {
-	render() {
-		return (
-			<button onClick={this.props.handleClick} className={styles.btn}></button>
-		);
-	}
-}
+
+const Btn = (props) => (
+	<button onClick={props.handleClick} className={styles.btn}></button>
+);
 
 export default Btn;
