a2aea527f794756e466983012ef9fa025e8febe3
aaaaargZombies <ahberate@riseup.net>
2021-11-25 17:17:09 +0000

    tweak info text

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -4,7 +4,7 @@ const Info = () => (
 	<div className={styles.info}>
 		<p>Welcome to the Adam Ondra Noise Production Centre.</p>
 		<p>
-			Watching a climbing vid and the person doesn't look like they're trying
+			Watching a climbing video where someone's not trying
 			hard enough? Can't find a vocalist for your metal band? Want to generate
 			some modesty noise while using a public toilet?
 		</p>
